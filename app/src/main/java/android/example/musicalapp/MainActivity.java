2a2ffd809a9447d23af5e8a02cb72a0bd49c39c0
Sergio Cunha <sergio.cunha-ext@viavarejo.com.br>
2019-04-05 17:35:25 -0300

    Criação Activity

--- app/src/main/java/android/example/musicalapp/MainActivity.java
+++ app/src/main/java/android/example/musicalapp/MainActivity.java
@@ -1,7 +1,10 @@
 package android.example.musicalapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void abrirFavoritas (View view){
+        Intent favoritas = new Intent(this, FavoritasActivity.class);
+        startActivity(favoritas);
+    }
+
+    public void abrirDowloads (View view){
+        Intent dowloads = new Intent(this, DowloadsActivity.class);
+        startActivity(dowloads);
+    }
+
+    public void abrirAlbuns (View view){
+        Intent albuns = new Intent(this, AlbunsActivity.class);
+        startActivity(albuns);
+    }
+
+    public void abrirArtistas (View view){
+        Intent artistas = new Intent(this, ArtistasActivity.class);
+        startActivity(artistas);
     }
 }
